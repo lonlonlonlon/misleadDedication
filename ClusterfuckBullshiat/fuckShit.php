3a511413dd8ae6076f8c49667d6f4efdacfbc916
lonlonlonlon <olbrueck.l99@gmail.com>
2022-04-28 09:25:59 +0200

    mazeMongoConsole

--- ClusterfuckBullshiat/fuckShit.php
+++ ClusterfuckBullshiat/fuckShit.php
@@ -5,7 +5,11 @@ class FuckShit
 
     public function start()
     {
-
+        $a=0;
+        while (1) {
+            echo($a."\n");
+            $a+=1;
+        }
     }
 }
 
--- MazeMongoConsole/src/Display.php
+++ MazeMongoConsole/src/Display.php
@@ -7,7 +7,16 @@ class Display
     {
     }
 
-    public function loadMap(string $string)
+    public function loadMap(string $mapName)
     {
+        try {
+            echo(system('pwd'));
+            @$picContent = file_get_contents("resources/maps/$mapName.pic");
+            @$jsonContent = file_get_contents("resources/maps/$mapName.json");
+        } catch (Exception $exception) {
+            // dunnow, maybee error handler klasse oder sou?
+            echo("fatal Error du Honk\n");
+        }
+
     }
 }
