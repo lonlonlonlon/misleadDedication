30ffbea1a6c7427b2d4bbf6e313a68d22e586e58
lonlonlonlon <olbrueck.l99@gmail.com>
2022-05-25 11:41:18 +0200

    LFify

--- Tools/LFify.php
+++ Tools/LFify.php
@@ -2,23 +2,56 @@
 
 class LFify
 {
+    private $path;
+
     public function __construct($argv)
     {
+        var_dump($argv);
         foreach ($argv as $index => $arg) {
             if (!$index === 0) {
                 $this->parseArg($arg, $index);
             }
         }
+        $this->doMagic();
     }
 
     private function parseArg($arg, $index)
     {
+        echo("$index: $arg\n");
         if($index === 1) {
             $this->path = $arg;
-        } else {
-            // andere optionen
         }
     }
+
+    private function doMagic()
+    {
+        recur($this->path);
+    }
+}
+
+function recur($path)
+{
+    echo("$path\n");
+    function doStuff($path) {
+        @$dirContent = scandir($path);
+        if(empty($dirContent)) {
+            echo("error reading dir $path\n");
+            return;
+        }
+        foreach ($dirContent as $item) {
+            if ($item == "." || $item == "..") {
+                continue;
+            }
+            // check if dir, if yes recur
+            if (is_dir($path . $item)) {
+                doStuff($path . $item . "/");
+            }
+            // check if file, if yes LFify it
+            system("dos2unix $path$item");
+            echo("LFified $path$item\n");
+        }
+    }
+    doStuff($path);
 }
 
 $lfify = new LFify($argv);
