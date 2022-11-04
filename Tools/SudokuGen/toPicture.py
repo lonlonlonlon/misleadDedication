fff3664e12cd8f9902961c4bdc3afbf1ce9d112c
Leon.Olbrueck <olbrueck.l99@gmail.com>
2022-11-04 12:39:55 +0100

    Sudoku Gen wip

--- Tools/SudokuGen/toPicture.py
+++ Tools/SudokuGen/toPicture.py
@@ -10,9 +10,10 @@ clock = pygame.time.Clock()
 
 FPS = 15  # How many times the screen will update per second
 
-width = 1000
-height = 1414
-border = 5
+width = 10000
+height = 14140
+border = (width/100)*3
+lineStrength = (width/1000)*2
 
 hPart = height/3
 wPart = width/2
@@ -29,6 +30,19 @@ right3 = pygame.Rect((wPart*1)+border, (hPart*2)+border, (wPart*2)-border, (hPar
 rects = [left1, left2, left3, right1, right2, right3]
 
 pic = pygame.Surface((width, height))
+pic.fill((255,255,255))
 
-for i in len(rects):
-    pygame.draw.line()
+for i in range(len(rects)):
+    print(i)
+    rect = rects[i]
+    (x, y, xx, yy) = rect
+    pygame.draw.line(pic, (0, 0, 0), (x, y), (x, yy), lineStrength)
+    pygame.draw.line(pic, (0, 0, 0), (x, y), (xx, y), lineStrength)
+    pygame.draw.line(pic, (0, 0, 0), (xx, yy), (x, yy), lineStrength)
+    pygame.draw.line(pic, (0, 0, 0), (xx, yy), (xx, y), lineStrength)
+
+    pygame.draw.line(pic, (0, 0, 0), (x/3, y), (x/3, yy), lineStrength)
+    # TODO: eigene surfaces für jedes sudoku, nacher auf pic blitten, sonst Koordinaten AA
+    pygame.draw.line(pic, (0, 0, 0), ((x/3)*2, y), ((x/3)*2, yy), lineStrength)
+
+pygame.image.save(pic, 'tmp.png', 'png')
