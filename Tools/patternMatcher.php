49544e34ecedb9707cf52f192fe9a58873bdfd84
lonlonlonlon <olbrueck.l99@gmail.com>
2023-11-07 17:04:59 +0100

    pattern Matcher // if (addressees is { Addresses: { } })

---
+++ Tools/patternMatcher.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class PatternMatcher {
+    public static function matchPattern(object $var, string $jsonPattern) {
+        $refObj = json_decode($jsonPattern);
+        // wip
+    }
+}
